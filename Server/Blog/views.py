dabf1a465a3624a7893474d0013367d2583a6c9e
Ezequiel Manfredi <ezemanfre@gmail.com>
2021-12-12 16:21:05 -0300

    [add]: home view and header abstraction

--- Server/Blog/views.py
+++ Server/Blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def Home(request):
+    ctxLocal = {}
+    #----------------------------------------
+    # obtener los destacados para el carusel
+    #----------------------------------------
+    if request.user.is_authenticated:
+        #----------------------------------------
+        # obtener la imagen del perfil
+        #----------------------------------------
+        ctxLocal['user'] = request.user
+    ctxLocal['range'] = range(1,19)
+    return render(request,'home.html',ctxLocal)
