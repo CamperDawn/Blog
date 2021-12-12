51548409e82fc671e3df19a9fa9d0eeedea41fc7
Ezequiel Manfredi <ezemanfre@gmail.com>
2021-12-12 16:11:38 -0300

    [add]: url paths the blog will use

--- Server/Blog/urls.py
+++ Server/Blog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from django.conf import settings
+from .views import Config, CreatePost, Detais, Home, LoginRegister, Profile, Search
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', Home,name='home_page'),
     
+    path('search/', Search),
+    
+    path('details/<int:id_post>/', Detais),
+    
+    path('profile/<str:user_name>/', Profile),
+    path('profile/<str:user_name>/create/', CreatePost),
+    path('profile/<str:user_name>/config/', Config),
+    
+    path('auth/<path:path>/', LoginRegister),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
