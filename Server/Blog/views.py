f2361bc2ca6551080fc4af66a6ed734664b0f0e9
Ezequiel Manfredi <ezemanfre@gmail.com>
2021-12-12 16:32:36 -0300

    [add]: view schema

--- Server/Blog/views.py
+++ Server/Blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def Home(request):
     ctxLocal = {}
@@ -12,3 +13,24 @@ def Home(request):
         ctxLocal['user'] = request.user
     ctxLocal['range'] = range(1,19)
     return render(request,'home.html',ctxLocal)
+
+def Search(request):
+    pass
+
+def LoginRegister(request,path):
+    pass
+
+def Detais(request,id_post):
+    pass
+
+@login_required(login_url='/auth/login/',redirect_field_name='next')
+def Profile(request,user_name):
+    pass
+
+@login_required(login_url='/auth/login/',redirect_field_name='next')
+def CreatePost(request,user_name):
+    pass
+
+@login_required(login_url='/auth/login/',redirect_field_name='next')
+def Config(request,user_name):
+    pass
